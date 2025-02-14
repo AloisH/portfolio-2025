27f43d37ea13ff77e49d3395fc5ea355c8499b24
Alois Heloir <alois.heloir@live.fr>
2025-02-14 10:34:03 +0100

    feat: rename lead to tech lead

--- components/section/HeroSection.vue
+++ components/section/HeroSection.vue
@@ -3,7 +3,7 @@
     <h1 class="text-gray-500 font-semibold py-2 px-4">HeroSection</h1>
     <div class="px-4 py-32">
       <h1 class="text-4xl font-semibold">Aloïs Heloir</h1>
-      <h2 class="text-xl">Teamlead & Senior Fullstack Developer</h2>
+      <h2 class="text-xl">Techlead & Senior Fullstack Developer</h2>
       <div class="mt-4 flex gap-4">
         <a href="#works">
           <button class="bg-black hover:bg-gray-800 text-white px-4 py-2">
